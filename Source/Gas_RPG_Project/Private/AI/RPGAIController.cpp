f3b5652b9bf73ee779060d82f28004adc209b211
Arthur Zakirov <zakirofartur@gmail.com>
2025-02-06 23:50:38 +0500

    Added AI module to the project

---
+++ Source/Gas_RPG_Project/Private/AI/RPGAIController.cpp
@@ -0,0 +1,5 @@
+// Copyright Arthur "Zer0HeaD" Zakirov
+
+
+#include "AI/RPGAIController.h"
+
---
+++ Source/Gas_RPG_Project/Public/AI/RPGAIController.h
@@ -0,0 +1,20 @@
+// Copyright Arthur "Zer0HeaD" Zakirov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "RPGAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_PROJECT_API ARPGAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+
+
+};
